5d274a06867eb3e1c31b73c0be2002fb19abb180
migueldev <miguel11psilva@gmail.com>
2025-03-05 18:25:12 -0300

    teste2miguel

--- src/Model/cliente.java
+++ src/Model/cliente.java
@@ -10,5 +10,42 @@ package Model;
  * @author LENOVO
  */
 public class cliente {
+     private int id;
+    private String nome;
+    private double preco;
+    private String tipo;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+    
     
 }
